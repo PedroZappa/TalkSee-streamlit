26f22329e6ebbb50856ebbb0ff768bafe8a9ac92
PedroZappa <pedrogzappa@gmail.com>
2023-06-13 12:04:09 +0100

    UPDATE: .env / CUDA check

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+from dotenv import load_dotenv
 import streamlit as st
 import whisper 
+import time
+import os
+import torch
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Setup Model Storage
+models_dir = os.getenv("MODELS_DIR")
 
 
 def main():
     # Streamlit UI: Title
     st.title("TalkSee : 🗣 ⇢ 👀")
 
+    # Check if CUDA is available
+    torch.cuda.is_available()
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    st.sidebar.text(f"Torch running on {DEVICE}")
     
     # Get user input
     ## Select Input Mode
     input_type = st.sidebar.radio(
-        'Input Mode',
+        'Select Input Mode',
         ('Mic', 'File')
     )
     
@@ -22,6 +36,7 @@ def main():
         ## Upload Audio file w/ Streamlit
         audio_file = st.file_uploader(
             "Upload Audio File", 
+            # Supported file types
             type=["wav", "mp3", "m4a"]
         )
     
@@ -30,7 +45,7 @@ def main():
     ## Select model size
     whisper_selected = st.sidebar.selectbox(
         'Available Multilingual Models',
-        ('tiny', 'base', 'small', 'medium', 'large', 'tiny.en', 'base.en', 'small.en', 'medium.en'),
+        ('tiny', 'base', 'small', 'medium', 'large', 'large-v2'),
         help="""
             |  Size  | Parameters | English-only model | Multilingual model | Required VRAM | Relative speed |
             |:------:|:----------:|:------------------:|:------------------:|:-------------:|:--------------:|
@@ -41,6 +56,18 @@ def main():
             | large  |   1550 M   |        N/A         |      `large`       |    ~10 GB     |       1x       |
         """ 
     ) 
+    
+    ## Check if select model exists in models directory
+    # model_file = os.path.join(models_dir, whisper_selected")
+    # if not os.path.exists(model_file):
+    #     st.warning(f"Model {whisper_selected} not found in {models_dir}. Downloading...")
+    
+    ## Load user selected model
+    # model = whisper.load_model(
+    #     whisper_selected,
+    #     download_root=models_dir
+    # )
+    # st.text(f"Whisper {whisper_selected} model loaded")
     
     
     # Transcribe audio file
